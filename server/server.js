b1556958b7b69c7b3a250a98973846f6f4ed3e55
Anushka Bachhav <131550798+Anushka040604@users.noreply.github.com>
2024-08-13 00:00:52 +0530

    Updated server.js

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const app = express();
 
 // Middleware
 app.use(cors());
-app.use(express.json()); // Parse JSON bodies
+app.use(express.json());
 app.use('/api/flashcards', flashcardRoutes);
 
 const PORT = process.env.PORT || 5000;
